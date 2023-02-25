d564cd650e521326c85c63bd74b0eb1a53f704e9
Mavik <mavik01@yandex.ru>
2023-02-25 21:13:42 +0300

    add add_metod 1,2

--- HomeWork/Program.cs
+++ HomeWork/Program.cs
@@ -1,1 +1,48 @@
-﻿
+﻿// Задача: Написать программу, которая из имеющегося массива строк формирует массив из строк, длина которых меньше
+// либо равна 3 символа. Первоначальный массив можно ввести с клавиатуры, либо задать на старте выполнения алгоритма.
+// При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
+// Пример:
+// ["hello", "2", "world", ":-)"] -> ["2", ":-)"]
+// ["1234", "1567", "-2", "computer science"] -> ["-2"]
+// ["Russia", "Denmark", "Kazan"] -> []
+
+
+// Метод преобразования строки в массив строк
+string[] StringToArr(string input)
+{
+    string[] arr = new string[input.Length];
+    int index = 0;
+    
+    for (int i = 0; i < input.Length; i++)
+    {
+        string temp = "";
+
+        while (input[i] != ' ')
+        {
+            if (i != input.Length - 1)
+            {
+                temp += input[i].ToString();
+                i++;
+            }
+            else
+            {
+                temp += input[i].ToString();
+                break;
+            }
+        }
+        arr[index] = temp;
+        index++;
+    }
+    return arr;
+}
+
+// Метод определения числа непустых значений в массиве строк
+int GetLengthArr(string[] arr)
+{
+    int count = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i] != null) count++;
+    }
+    return count;
+}
